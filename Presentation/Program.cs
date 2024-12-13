d49ef5621bc57752fb7a06e135c495370f8ba03a
duffiwer <duffiwer@gmail.com>
2024-12-13 03:03:59 +0300

    Create Program.cs

---
+++ Presentation/Program.cs
@@ -0,0 +1,27 @@
+﻿using BLL;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection; 
+
+namespace Presentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory()) 
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var serviceProvider = new ServiceCollection()
+                .AddSingleton<IConfiguration>(configuration) 
+                .AddSingleton<FamilyTreeRepository>()            
+                .AddSingleton<FamilyTreeService>()              
+                .AddSingleton<FamilyTreeApp>()                  
+                .BuildServiceProvider();
+
+            var app = serviceProvider.GetService<FamilyTreeApp>();
+            app.Run();
+        }
+    }
+}
